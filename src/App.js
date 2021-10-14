13f73b9dd2c7b6c72efafbbad25b557b3bf60180
Kajal Manohar Nimje <kajalnimje1998@gamail.com>
2021-10-14 12:09:42 +0530

    hii

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import "./App.css"
 
-function App() {
+const App = () => {
+   const [input, setinput] = useState("");
+   const [items, setitems] = useState([]);
+   const additems=()=>{
+     if(!input){
+
+     }
+     else{
+       setitems([...items,input]);
+       setinput('');
+     }
+   }
+
+   const delet=(id)=>{
+    const updated = items.filter((elem,ind)=>{
+      return ind!==id ;
+    
+    });
+    setitems(updated );
+  
+  }
+
+   const remove=()=>{
+     setitems([]);
+
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="main">
+
+     
+      <div className="sub">
+        <img src="./todo.jpg"/>
+      <h1> To-Do-List ✌️</h1>
+      {/* add items */}
+       <div className="add-items">
+         <input 
+         value={input} 
+         onChange={(e)=>setinput(e.target.value)}
+         type="text" 
+         placeholder="📝 Add Items"/>
+         <h3 
+         onClick={additems} className="addbtn"  title="add items"> + </h3>
+       </div>
+        
+
+      {/* show items */}
+      <div className="show-items" >
+        {
+          items.map((ele,ind)=>{
+            return(
+              <>
+              <div className="each-items" key={ind}>
+              <h3> {ele} </h3>
+              <button  className="btn" onClick={()=>delet(ind)}className="btn" title="Delet items">  Delet </button>
+              </div>
+              
+              </>
+
+            )
+          })
+        }
+        
+      </div>
+
+      {/* remove */}
+      <button onClick={remove} className="remove"> Remove all </button>
+      </div>
+
     </div>
-  );
+
+      
+    </>
+  )
 }
 
-export default App;
+export default App
